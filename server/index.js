c20a8a3eb80eb0e489310ee32ac383f929db78c9
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2021-10-22 17:18:47 +0100

    updating to remove mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const PORT = process.env.PORT || 8080
-const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
 const routes = require('./src/routes/api');
 
 app.use(cors());
@@ -12,15 +10,6 @@ app.use('/static', express.static('public'));
 
 app.use('/', routes);
 
-mongoose.connect(process.env.DBURI, {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function() {
-  console.log('DB connected');
-  app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
-
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,32 +1,12 @@
-const User = require('../../../db/models/user')
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({message: 'Hello World!'})
-})
+  console.log('Hello World!');
+});
 
-router.post('/login', ( req, res ) => {
-  res.send('This route is for logging in')
-})
-
-router.post('/register', ( req, res ) => {
-  const email = req.body.email;
-  const userName = req.body.userName;
-  const password = req.body.password;
-  const newUser = new User({
-    email,
-    userName,
-    password
-  })
-  newUser.save();
-    if (res.statusCode === 200) {
-        res.send({message: 'User successfully registered'});
-    }
-})
-
-router.get('/dashboard', ( req, res ) => {
-  res.send('This route is for the User\'s dashboard')
-})
+router.get('/dashboard', (req, res) => {
+  res.send("This route is for the User's dashboard");
+});
 
 module.exports = router;
