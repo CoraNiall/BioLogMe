976841ed02ed701075cfae16d8db4af98fc4dcc6
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2021-10-22 22:46:53 +0100

    more changes to forms

--- client/src/components/activityLog.jsx
+++ client/src/components/activityLog.jsx
@@ -5,11 +5,11 @@ export default function ActivityLog() {
   return (
     <Form.Group action='' method='post' className='activity-form'>
       <div className='activity-form'>
-        <Form.Label for='name'>Enter your name: </Form.Label>
+        <Form.Label htmlFor='name'>Enter your name: </Form.Label>
         <Form.Control type='text' name='name' id='name' required />
       </div>
       <div className='activity-form'>
-        <Form.Label for='email'>Enter your email: </Form.Label>
+        <Form.Label htmlFor='email'>Enter your email: </Form.Label>
         <Form.Control type='email' name='email' id='email' required />
       </div>
       <div className='activity-form'>
--- client/src/components/foodLog.jsx
+++ client/src/components/foodLog.jsx
@@ -5,7 +5,23 @@ export default function FoodLog() {
   return (
     <Form.Group action='' method='post' className='food-form'>
       <div className='food-form'>
-        <Form.Control type='submit' value='Log your Food!' />
+        <Form.Label htmlFor='breakfast'>Breakfast: </Form.Label>
+        <Form.Control type='text' name='breakfast' id='breakfast' />
+      </div>
+      <div className='food-form'>
+        <Form.Label htmlFor='lunch'>Lunch: </Form.Label>
+        <Form.Control type='lunch' name='lunch' id='lunch' />
+      </div>
+      <div className='food-form'>
+        <Form.Label htmlFor='dinner'>Dinner: </Form.Label>
+        <Form.Control type='dinner' name='dinner' id='dinner' />
+      </div>
+      <div className='food-form'>
+        <Form.Label htmlFor='snacks'>Snacks: </Form.Label>
+        <Form.Control type='snacks' name='snacks' id='snacks' />
+      </div>
+      <div className='food-form'>
+        <Form.Control type='submit' value='Log your food!' />
       </div>
     </Form.Group>
   );
