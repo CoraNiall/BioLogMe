10a99f70f10daa96b50d6ecb4b131535b977139d
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2021-12-04 15:28:02 +0000

    updating db connection with functions

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoclient = require('../db/conn');
+const db = require('../db/conn');
 const routes = require('./src/routes/api');
 
 const PORT = process.env.PORT || 8080;
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use('/static', express.static('public'));
 
 app.use('/', routes);
+
+db.connectToServer();
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
