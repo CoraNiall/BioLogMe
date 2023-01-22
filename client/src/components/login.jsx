ed9c82f15591089bb18d31abc550057a72919cd3
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2023-01-22 15:39:50 +0000

    adding some changes

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-import '../styles/log.css';
+import "../styles/log.css";
 
 export default function Login() {
   const getCookie = () => {
@@ -21,51 +21,50 @@ export default function Login() {
   const [authenticated, setAuthenticated] = useState(getCookie() || false);
 
   const [user, setUser] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const handleSubmit = async e => {
     e.preventDefault();
 
     await axios
-      .post('http://localhost:8080/login', {
+      .post("http://localhost:8080/login", {
         user,
       })
       .then(res => {
-        console.log(res.data || 'User not found');
+        console.log(res.data || "User not found");
         let loggedInUser = res.data.user;
         setCookie(loggedInUser);
         setAuthenticated(true);
       })
       .catch(e => {
-        console.log('User not found', e);
+        console.log("User not found", e);
       });
   };
 
   return (
-    <div className='container-sm col-md-8'>
+    <div className="container-sm col-md-8">
       <h2>Please log in to access BioLogMe</h2>
-      <form action='/login' method='post' onSubmit={handleSubmit}>
-        <div className='mb-3'>
+      <form action="/login" method="post" onSubmit={handleSubmit}>
+        <div className="mb-3">
           <br />
           <input
-            type='email'
-            name='email'
-            id='email'
-            className='form-control'
-            placeholder='Enter email'
+            type="email"
+            name="email"
+            id="email"
+            className="form-control"
+            placeholder="Enter email"
             value={user.email}
             onChange={e => setUser({ ...user, email: e.target.value })}
             required
           />
           <br />
           <input
-            type='text'
-            name='password'
-            id='password'
-            className='form-control'
-            placeholder='Enter password'
+            type="password"
+            name="password"
+            id="password"
+            className="form-control"
             value={user.password}
             onChange={e => setUser({ ...user, password: e.target.value })}
             required
@@ -73,9 +72,9 @@ export default function Login() {
           <br />
           <div>
             <input
-              className='btn btn-primary user-details-btn'
-              type='submit'
-              value='Submit'
+              className="btn btn-primary user-details-btn"
+              type="submit"
+              value="Submit"
             />
           </div>
         </div>
