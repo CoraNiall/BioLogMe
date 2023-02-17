4337631196ecfe80a5a0db8d83872bc8c7283343
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2023-02-17 16:15:28 +0000

    cannot remember what I did with this

--- server/db/userDetails.js
+++ server/db/userDetails.js
@@ -1,25 +1,23 @@
-import { createUniqueUser } from '../src/utils.js';
+import { createUniqueUser } from "../src/utils.js";
 
 async function registerUser(mongoclient, newUser) {
   const { name, email, password } = newUser;
   try {
     await mongoclient
-      .db('biologme')
-      .collection('users')
+      .db("biologme")
+      .collection("users")
       .insertOne({
         _id: createUniqueUser(),
         userName: name,
         email: email,
         password: password,
         registrationDate: new Date(Date.now())
-          .toLocaleString('en-gb')
-          .split(',')[0],
+          .toLocaleString("en-gb")
+          .split(",")[0],
       });
-    console.log('New user added');
+    console.log("New user added");
   } catch (e) {
     console.error(e);
-  } finally {
-    await mongoclient.close();
   }
 }
 
@@ -27,67 +25,59 @@ async function getUser(mongoclient, user) {
   const { email, password } = user;
   try {
     const userObj = await mongoclient
-      .db('biologme')
-      .collection('users')
+      .db("biologme")
+      .collection("users")
       .findOne({ email: email, password: password });
 
     if (!userObj) {
-      console.log('User not found');
+      console.log("User not found");
     } else {
       return userObj;
     }
   } catch (e) {
     console.error(e);
-  } finally {
-    await mongoclient.close();
   }
 }
 
 async function updateUserById(mongoclient, userId, updateValue) {
   try {
     const user = await mongoclient
-      .db('biologme')
-      .collection('users')
+      .db("biologme")
+      .collection("users")
       .updateOne({ _id: userId }, { $set: updateValue });
     console.log(user);
     return user;
   } catch (e) {
     console.error(e);
-  } finally {
-    await mongoclient.close();
   }
 }
 
 async function deleteUserById(mongoclient, userId) {
   try {
     const user = await mongoclient
-      .db('biologme')
-      .collection('users')
+      .db("biologme")
+      .collection("users")
       .deleteOne({ _id: userId });
-    console.log('User succesfully deleted from DB');
+    console.log("User succesfully deleted from DB");
   } catch (e) {
     console.error(e);
-  } finally {
-    await mongoclient.close();
   }
 }
 
 async function checkUserExists(mongoclient, email) {
   try {
     const userObj = await mongoclient
-      .db('biologme')
-      .collection('users')
+      .db("biologme")
+      .collection("users")
       .findOne({ email: email });
 
     if (!userObj) {
-      console.log('User not found');
+      console.log("User not found");
     } else {
       return userObj;
     }
   } catch (e) {
     console.error(e);
-  } finally {
-    await mongoclient.close();
   }
 }
 
